70c6d3d7cf3d317dabb71f9bc4b2e6a0c84a5b10
Aniruddha Das <aniruddhadas9@gmail.com>
2014-09-18 23:59:35 +0530

    added

---
+++ war/WEB-INF/src/com/candi/jpa/Employee.java
@@ -0,0 +1,45 @@
+package com.candi.jpa;
+
+import com.google.appengine.api.datastore.Key;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Key key;
+
+    private String firstName;
+    private String lastName;
+    private Date hireDate;
+
+    public Key getKey() {
+        return key;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getHireDate() {
+        return hireDate;
+    }
+    public void setHireDate(Date hireDate) {
+        this.hireDate = hireDate;
+    }
+}
